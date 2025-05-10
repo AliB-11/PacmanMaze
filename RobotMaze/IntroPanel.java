39e96411fe0c581d274a856e61be23c3dec68a54
Ali Bandali <alimbandali@gmail.com>
2025-05-10 17:08:49 -0400

    Update IntroPanel.java

--- RobotMaze/IntroPanel.java
+++ RobotMaze/IntroPanel.java
@@ -8,65 +8,62 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 
-/** 
-* The IntroPanel class 
-* <p> This class creates a intro  panel for the user to interact with </p>
-* <p> it prompts the user to play and gives it three buttons to choose from </p>
-* @author 
-* @version 1.0
-* @since 2023-01-24
-*/
+/**
+ * The IntroPanel class
+ * This class creates a intro panel for the user to interact with
+ * it prompts the user to play
+ */
 
 public class IntroPanel extends JPanel implements ActionListener {
   // Fields
 
-  /**
-  * small is JButton that generates a 5 x 5 map  
-  */
   JButton small;
-  /**
-  * medium is a JButton that generates a 8 x 8 map 
-  */
+
   JButton medium;
-  /**
-  * large is a JButton that generates a 10 x 10 map 
-  */
+
   JButton large;
-  /**
-  * intro is a JTextField that prompts the user
-  */
+
   JTextField intro;
 
-  // Contructor 
+  JLabel start;
+
+  // Contructor
 
   /**
-   * <p> This constructor will create all the piece for the intro panel </p>
+   * This constructor will create all the piece for the intro panel
    */
   public IntroPanel() {
 
     // set the background colour to blue
-    this.setBackground(Color.blue);
+    this.setBackground(Color.YELLOW);
     setLayout(null);
 
     // create all the buttons and the text on them
-    small = new JButton("5 X 5");
-    medium = new JButton("8 X 8");
-    large = new JButton("10 X 10");
+    small = new JButton("Small");
+    medium = new JButton("Medium");
+    large = new JButton("Large");
+    start = new JLabel();
     // create the textfield and write the prompt
-    intro = new JTextField("Select What Size Map to Play");
+    intro = new JTextField("        Select the Size Map to Play");
+    intro.setEditable(false);
 
     // set the bounds of the buttons and the textfield
-    intro.setBounds(25, 5, 250, 50);
-    small.setBounds(100, 70, 100, 50);
-    medium.setBounds(100, 140, 100, 50);
-    large.setBounds(100, 210, 100, 50);
+    intro.setBounds(33, 140, 250, 50);
+    small.setBounds(115, 210, 100, 50);
+    medium.setBounds(115, 280, 100, 50);
+    large.setBounds(115, 350, 100, 50);
+    start.setBounds(15, 15, 300, 165);
+    start.setIcon(new ImageIcon("images/Start.png"));
 
     // add all the piece to the panel
     add(intro);
     add(small);
     add(medium);
     add(large);
+    add(start);
 
     // add actions listeners to the three buttons
     small.addActionListener(this);
@@ -74,10 +71,8 @@ public class IntroPanel extends JPanel implements ActionListener {
     large.addActionListener(this);
   } // END OF CONSTRUCTOR
 
-  
-  /** 
-   * <p> actionPerformed method looks at which size map the user wants to play</p>
-   * @param e
+  /**
+   * actionPerformed method looks at which size map the user wants to play
    */
   public void actionPerformed(ActionEvent e) {
     // declare and intilize variable for size
@@ -85,26 +80,22 @@ public class IntroPanel extends JPanel implements ActionListener {
     // delcare and initlize varible to look at which button was clicked
     String button = e.getActionCommand();
 
-      // if the small button was clickd
-    if (button.equals("5 X 5")) {
-      // set size to 5
+    // if the small button was clickd
+    if (button.equals("Small")) {
       size = 5;
       // if the medium button was clicked
-    } else if (button.equals("8 X 8")) {
-      // set size to 8
+    } else if (button.equals("Medium")) {
       size = 8;
       // if the large button was clicked
-    } else if (button.equals("10 X 10")) {
-      // set size to 10
+    } else if (button.equals("Large")) {
       size = 10;
     } else {
-      // NO ELSE
+
     }
 
-    // set the variable in main class to variable size here
     Main.size = size;
     // call setMapPanelVisible method in main class
     Main.setMapPanelVisible();
 
-  }// END OF ACTIONPERFORMED METHOD
+  }
 } // END OF CLASS
